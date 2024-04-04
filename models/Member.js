8e9a9148379cd20f0d80e05fb8e74b9d720e621f
Sherzod19911 <sherzod.baratov.1991@mail.ru>
2024-04-04 23:41:44 +0900

    feat: develop login page business logic

--- models/Member.js
+++ models/Member.js
@@ -1,5 +1,7 @@
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake"); 
+const assert = require("assert");
+const bcrypt = require("bcryptjs");
 
 
 class Member {
@@ -15,8 +17,8 @@ async signupData(input) {
         console.log("new_member::::", new_member)  
         let result;         
       try{
-        const result = await new_member.save();
-        console
+         result = await new_member.save();
+        console.log("result::", result)
       } catch(mongo_err) {    
         console.log(mongo_err);
       throw new Error(Definer.auth_err1);
@@ -24,9 +26,29 @@ async signupData(input) {
 
   
       
-    // result.mb_password = "";
+     result.mb_password = "";
      return result;
 
+    } catch(err) {
+      throw err;
+    }
+   }
+   async loginData(input) {
+    try{
+     const member = await this.memberModel
+     .findOne(
+      {mb_nick: input.mb_nick}, 
+      {mb_nick:1,mb_password: 1}
+      )
+     .exec();
+     assert.ok(member,Definer.auth_err3);
+     //console.log(member);
+      const isMatch = input.mb_password===member.mb_password;
+      assert.ok(isMatch,Definer.auth_err4);
+      return await this.memberModel.findOne({
+        mb_nick:input.mb_nick
+      })
+      .exec();
     } catch(err) {
       throw err;
     }
