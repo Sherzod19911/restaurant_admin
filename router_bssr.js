086994d2945c014bdac457d88017aff447cae049
Sherzod19911 <sherzod.baratov.1991@mail.ru>
2024-04-07 20:02:32 +0900

    feat:restaurant user getting restaurant related products

--- router_bssr.js
+++ router_bssr.js
@@ -17,11 +17,11 @@ router_bssr.post("/signup", restaurantController.signupProcess);
 router_bssr.get("/login", restaurantController.getLoginMyRestaurant);
 router_bssr.post("/login", restaurantController.loginProcess);
 
-
+     
 router_bssr.get("/logout", restaurantController.logout);
 
 router_bssr.get("/check-me", restaurantController.checkSessions);
-router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantProducts);
 
 router_bssr.post("/products/create",restaurantController.validateAuthRestaurant,
 uploader_product.array("product_images",2),productController.addNewProduct);
